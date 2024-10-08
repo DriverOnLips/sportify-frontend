6751be6e4b34c9e643d62a66767522ebfda1d7db
Roman Sulaymanov <92111647+DriverOnLips@users.noreply.github.com>
2024-10-08 22:46:11 +0300

    update cfgs

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,6 @@ function App() {
 
 	return (
 		<>
-
-
-      
 			<div>
 				<a
 					href='https://vitejs.dev'
@@ -42,6 +39,7 @@ function App() {
 					Edit <code>src/App.tsx</code> and save to test HMR
 				</p>
 			</div>
+
 			<p className='read-the-docs'>
 				Click on the Vite and React logos to learn more
 			</p>
