72483e3a69831a38f5d68398d577b4c785cd7780
Roman Sulaymanov <92111647+DriverOnLips@users.noreply.github.com>
2024-10-20 11:10:07 +0300

    feat: добавил env context

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,36 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Helmet } from 'react-helmet-async';
-import Sidebar from './components/Menu/Menu.tsx';
 import EventPage from 'pages/EventPage/EventPage.tsx';
 import EventsList from 'pages/EventsList/EventsList.tsx';
-import { UserProvider } from './contexts/User/userContext.tsx';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Sidebar from './components/Menu/Menu.tsx';
+
 import './App.scss';
 
 function App() {
-	const apiKey = import.meta.env.VITE_YANDEX_MAP_API_KEY;
-
 	return (
 		<div id='app'>
-			<Helmet>
-				<script
-					src={`https://api-maps.yandex.ru/2.1/?lang=ru_RU&apikey=${apiKey}`}
-					type='text/javascript'
-				/>
-			</Helmet>
-
-			<UserProvider>
-				<BrowserRouter basename='/'>
-					<div id='content'>
-						<Routes>
-							<Route
-								path='/'
-								element={<EventsList />}
-							/>
-							<Route
-								path='/event/:id'
-								element={<EventPage />}
-							/>
-						</Routes>
-					</div>
-					<Sidebar />
-				</BrowserRouter>
-			</UserProvider>
+			<BrowserRouter basename='/'>
+				<div id='content'>
+					<Routes>
+						<Route
+							path='/'
+							element={<EventsList />}
+						/>
+						<Route
+							path='/event/:id'
+							element={<EventPage />}
+						/>
+						{/* <Route
+						path='/not_found'
+						element={<NotFound />}
+					/>
+					<Route
+						path='*'
+						element={<NotFound />}
+					/> */}
+					</Routes>
+				</div>
+				<Sidebar />
+			</BrowserRouter>
 		</div>
 	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HelmetProvider } from 'react-helmet-async';
 
 import App from './App.tsx';
+import { EnvProvider } from './contexts/EnvContext.tsx';
+import { UserProvider } from './contexts/User/userContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<HelmetProvider>
-			<App />
-		</HelmetProvider>
+		<EnvProvider>
+			<UserProvider>
+				<App />
+			</UserProvider>
+		</EnvProvider>
 	</React.StrictMode>,
 );
