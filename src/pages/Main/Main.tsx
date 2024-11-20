ca014eea69cfcfc56369340c438d95e25c2ba092
Roman Sulaymanov <92111647+DriverOnLips@users.noreply.github.com>
2024-11-20 16:32:45 +0300

    feat: добавил переход на страницы через тг

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,15 +1,33 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { selectTGWebAppStartPage } from '../../states/TGWebApp/TGWebAppState.ts';
+import { selectTGWebAppStartPage } from 'states/TGWebApp/TGWebAppState.ts';
 import { useNavigate } from 'react-router-dom';
+import {
+	convertStringToTgStartPage,
+	TgStartPage,
+} from 'types/enums/TgStartPage.ts';
 
 const MainPage: React.FC = () => {
 	const tgStartPage = useSelector(selectTGWebAppStartPage);
 	const navigate = useNavigate();
 
+	const navigateToStartPage = (page: TgStartPage) => {
+		switch (page) {
+			case TgStartPage.EventCreate:
+				navigate('/events-create');
+				break;
+			case TgStartPage.Events:
+				navigate('/events');
+				break;
+			case TgStartPage.Map:
+				navigate('/map');
+				break;
+		}
+	};
+
 	useEffect(() => {
-		if (tgStartPage == 'create_event') {
-			navigate('/events-create');
+		if (tgStartPage !== '') {
+			navigateToStartPage(convertStringToTgStartPage(tgStartPage));
 		}
 	}, [tgStartPage]);
 
